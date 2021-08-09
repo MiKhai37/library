bfaa28af4267f0c46fcdec3b57b658674c473756
MiKhai37 <mi.khai.thai@gmail.com>
2021-08-09 06:52:48 +0200

    add toggle read button

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-let myLibrary = [];
+let myLibrary = [{"author": "author 1", "title": "title 1", "nbPages": 100, "read": true},
+                 {"author": "author 2", "title": "title 2", "nbPages": 200, "read": false},
+                 {"author": "author 3", "title": "title 3", "nbPages": 400, "read": true},
+                 {"author": "author 4", "title": "title 4", "nbPages": 100, "read": false}];
 
 function Book(author, title, nbPages, read) {
     this.author = author;
@@ -18,34 +21,44 @@ function addBookToLibrary(library, author, title, nbPages, read) {
     library.push(new Book(author, title, nbPages, read));
 }
 
+function removeBookToLibrary(library, cardID) {
+    const book = document.getElementById(cardID)
+    library.pop(cardID)
+}
+
 function renderBookCards(containerID, library) {
     const container = document.querySelector('#' + containerID);
     for (let i = 0; i < library.length; i++) {
-        div = document.createElement('div');
+
+        //If the card already exists don't render them
+        if (document.getElementById(`card_${i}`)) continue;
+
+        const div = document.createElement('div');
         div.classList.add('card');
+        div.id = `card_${i}`
         container.appendChild(div);
 
-        containerCard = document.createElement('div');
+        const containerCard = document.createElement('div');
         containerCard.classList.add('container');
         div.appendChild(containerCard);
 
-        h4Title = document.createElement('h4');
+        const h4Title = document.createElement('h4');
         h4Title.textContent = library[i].title;
         containerCard.appendChild(h4Title);
 
-        pAuthor = document.createElement('p');
+        const pAuthor = document.createElement('p');
         pAuthor.textContent = library[i].author;
         containerCard.appendChild(pAuthor);
 
-        pPages = document.createElement('p');
+        const pPages = document.createElement('p');
         pPages.textContent = library[i].nbPages + " pages";
         containerCard.appendChild(pPages);
 
-        readBtn = document.createElement('button');
+        const readBtn = document.createElement('button');
         readBtn.classList.add('btn')
         containerCard.appendChild(readBtn);
 
-        readIcon = document.createElement('i');
+        const readIcon = document.createElement('i');
         readIcon.classList.add("fa");
         if (library[i].read) {
             readIcon.classList.add("fa-check");
@@ -58,13 +71,27 @@ function renderBookCards(containerID, library) {
         }
         readIcon.classList.add("icon");
         readBtn.appendChild(readIcon);
+        readBtn.addEventListener('click', () => {
+            library[i].read = !library[i].read
+            if (library[i].read) {
+                readIcon.classList.remove("fa-times");
+                readIcon.classList.add("fa-check");
+                readIcon.style.backgroundColor = "green"
+                readBtn.style.backgroundColor = "green"
+            } else {
+                readIcon.classList.remove("fa-check");
+                readIcon.classList.add("fa-times");
+                readIcon.style.backgroundColor = "orangered"
+                readBtn.style.backgroundColor = "orangered"
+            }
+        });
 
-        deleteBtn = document.createElement('button');
+        const deleteBtn = document.createElement('button');
         deleteBtn.classList.add("btn");
         deleteBtn.style.backgroundColor = "orangered";
         containerCard.appendChild(deleteBtn);
 
-        deleteIcon = document.createElement('i');
+        const deleteIcon = document.createElement('i');
         deleteIcon.classList.add("fa");
         deleteIcon.classList.add("fa-trash");
         deleteIcon.classList.add("icon");
@@ -82,16 +109,6 @@ function overlayOff() {
     document.getElementById("overlay").style.display = "none";
 } 
 
-function toggleRead() {
-    
-}
-
-addBookToLibrary(myLibrary, 'author 1', 'title 1', 100, true);
-addBookToLibrary(myLibrary, 'author 2', 'title 2', 200, false);
-addBookToLibrary(myLibrary, 'author 3', 'title 3', 300, false);
-addBookToLibrary(myLibrary, 'author 3', 'title 3', 300, false);
-addBookToLibrary(myLibrary, 'author 3', 'title 3', 300, false);
-
 renderBookCards('card-container', myLibrary);
 
 const newBtn = document.querySelector('#new-btn');
@@ -103,3 +120,14 @@ const cancelFormBtn = document.querySelector('#cancel-btn-form');
 cancelFormBtn.addEventListener('click', () => {
     overlayOff();
 });
+
+const addBookBtn = document.querySelector('#add-btn-form')
+addBookBtn.addEventListener('click', () => {
+    const title = document.querySelector("#form-title").value;
+    const author = document.querySelector("#form-author").value;
+    const pages = document.querySelector("#form-pages").value;
+    console.log('add');
+    addBookToLibrary(myLibrary, author, title, pages, false);
+    renderBookCards('card-container', myLibrary);
+    overlayOff();
+})
