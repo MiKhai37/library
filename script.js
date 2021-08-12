79114ae6c293953f3a6b4bd858234c5fbbe02850
MiKhai37 <mi.khai.thai@gmail.com>
2021-08-12 07:20:11 +0200

    Use innerHtml instead of multiple appendChild

--- script.js
+++ script.js
@@ -18,108 +18,46 @@ class library{
 }
 
 class Book {
-    constructor(author, title, nbPages, read) {
+    constructor(author, title, totPages, read) {
         this.author = author;
         this.title = title;
-        this.nbPages = nbPages
+        this.totPages = totPages
         this.read = read;
     }
     info() {
-        console.log(this.title, this.author, this.nbPages, this.read)
+        console.log(this.title, this.author, this.totPages, this.read)
     }
 }
 
 function render(containerID, library) {
     const container = document.querySelector('#' + containerID);
-    // Wipe Out the container
-    container.innerHTML = ""
+    // Wipe out the container
+    container.innerHTML = "";
     // Render all book in library, card style
     for (let i = 0; i < library.books.length; i++) {
+        const card = document.createElement('div');
+        card.classList.add('card')
+        card.innerHTML = `<h4>${library.books[i].title}</h4>
+                          <p>${library.books[i].author}</p>
+                          <p>${library.books[i].totPages} pages</p>`;
+        container.appendChild(card);
 
-        library.books[i].info();
+        const readBtn = document.createElement('button')
+        readBtn.classList.add('btn');
+        readBtn.innerHTML = '<i class="fa fa-check icon"></i>'
+        card.appendChild(readBtn);
 
-        const div = document.createElement('div');
-        const containerCard = document.createElement('div');
-        const h4Title = document.createElement('h4');
-        const pAuthor = document.createElement('p');
-        const pPages = document.createElement('p');
-        const readBtn = document.createElement('button');
-        const readIcon = document.createElement('i');
-        const deleteBtn = document.createElement('button');
-        const deleteIcon = document.createElement('i');
-
-        div.classList.add('card');
-        div.id = `card_${i}`;
-        div.dataset.id = i;
-        container.appendChild(div);
-
-        containerCard.classList.add('container');
-        div.appendChild(containerCard);
-        
-        h4Title.textContent = library.books[i].title;
-        containerCard.appendChild(h4Title);
-
-        pAuthor.textContent = library.books[i].author;
-        containerCard.appendChild(pAuthor);
-
-        pPages.textContent = library.books[i].nbPages + " pages";
-        containerCard.appendChild(pPages);
-        
-        readBtn.classList.add('btn')
-        containerCard.appendChild(readBtn);
-
-        
-        readIcon.classList.add("fa");
-        if (library.books[i].read) {
-            readIcon.classList.add("fa-check");
-            readIcon.style.backgroundColor = "green"
-            readBtn.style.backgroundColor = "green"
-        } else {
-            readIcon.classList.add("fa-times");
-            readIcon.style.backgroundColor = "orangered"
-            readBtn.style.backgroundColor = "orangered"
-        }
-        readIcon.classList.add("icon");
-        readBtn.appendChild(readIcon);
-        readBtn.addEventListener('click', () => {
-            library.books[i].read = !library.books[i].read
-            if (library.books[i].read) {
-                readIcon.classList.remove("fa-times");
-                readIcon.classList.add("fa-check");
-                readIcon.style.backgroundColor = "green"
-                readBtn.style.backgroundColor = "green"
-            } else {
-                readIcon.classList.remove("fa-check");
-                readIcon.classList.add("fa-times");
-                readIcon.style.backgroundColor = "orangered"
-                readBtn.style.backgroundColor = "orangered"
-            }
-        });
-
-        
-        deleteBtn.classList.add("btn");
-        deleteBtn.dataset.id = i;
-        deleteBtn.style.backgroundColor = "orangered";
-        containerCard.appendChild(deleteBtn);
-
-        
-        deleteIcon.classList.add("fa");
-        deleteIcon.classList.add("fa-trash");
-        deleteIcon.classList.add("icon");
-        deleteIcon.style.backgroundColor = "orangered"
-        deleteBtn.appendChild(deleteIcon);
-
-        deleteBtn.addEventListener('click', delCard)
+        const delBtn = document.createElement('button');
+        delBtn.classList.add('btn');
+        delBtn.innerHTML = '<i class="fa fa-trash icon"></i>';
+        card.appendChild(delBtn);
     }
 }
 
 function delCard() {
     const id = this.dataset.id;
-    
-    const container = document.querySelector('#card-container');
-    const div = document.querySelector(`#card_${id}`);
     myLibrary.delBook(id)
-    container.removeChild(div);
+    render('card-container', myLibrary)
 }
 
 //Functions to display or not the form overlay
